670bbdd742ed3c3ee305daa3b8b7f87ddea1d940
Brian Simiyu <briansimiyuj@gmail.com>
2022-10-15 18:17:50 +0300

    Prototype

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ function User(email, name) {
 
     this.online = false
 
+}
 
-    this.login = function(){
 
-        console.log(this.email, 'has logged in')
 
-    }
+User.prototype.login = function(){
+
+    this.online = true
+
+    console.log(this.email, 'has logged in')
+
+}
+
+
+
+User.prototype.logout = function(){
+
+    this.online = false
+
+    console.log(this.email, 'has logged out')
 
 }
 
